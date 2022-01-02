d03124562cf7f71b0b00eb8a2b2c8e8d693de12c
fernandoors <fernando.ors@outlook.com>
2022-01-01 22:50:08 -0300

    feat create a route for product

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,12 @@
+import productsRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
+
 const routes = Router();
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello' });
 });
+
+routes.use('/products', productsRouter);
 
 export default routes;
