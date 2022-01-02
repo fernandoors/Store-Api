30b055ada48b2ac2c395c983754e412ba0d664ac
fernandoors <fernando.ors@outlook.com>
2022-01-02 19:43:40 -0300

    feat crypt user password using bcrypt

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -18,11 +19,21 @@ export default class CreateUserService {
     if (emailExists) {
       throw new AppError('This e-mail address already exists');
     }
+    const hashedPassword = await this.cryptPassword(password);
 
-    const user = usersRepository.create({ name, email, password });
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     await usersRepository.save(user);
 
     return user;
+  }
+
+  private async cryptPassword(password: string): Promise<string> {
+    const hashedPassword = await hash(password, 8);
+    return hashedPassword;
   }
 }
