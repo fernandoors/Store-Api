a61da69810d17f13c70a33f438f964e218008048
fernandoors <fernando.ors@outlook.com>
2022-01-01 23:07:21 -0300

    feat add celebrate for data validations

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import 'express-async-errors';
+import { errors as celebrateErrors } from 'celebrate';
 import cors from 'cors';
 import routes from './routes';
 import errors from '@shared/middlewares/errors';
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(celebrateErrors());
 app.use(errors);
 
 app.listen(3333, () => {
