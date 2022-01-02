e04ec05393bc0471c1540385c4af1cd5800afb2b
fernandoors <fernando.ors@outlook.com>
2022-01-02 19:17:46 -0300

    fix change  user repository file name

--- src/modules/users/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Repository } from 'typeorm';
 import User from '../entities/User';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {
+export default class UserRepository extends Repository<User> {
   public async findByName(name: string): Promise<User | undefined> {
     const user = await this.findOne({ where: { name } });
     return user;
