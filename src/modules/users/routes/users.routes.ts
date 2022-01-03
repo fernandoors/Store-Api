f243a65d9e345f4355a5d7e059aeec0d7428dc4a
fernandoors <fernando.ors@outlook.com>
2022-01-03 17:10:03 -0300

    feat add user update to send avatar image

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,15 +1,31 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+import uploadSettings from '@config/upload';
 
 import UsersController from '../controllers/UsersController';
 import UserValidations from '../validations/UsersValidations';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
+
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
 const userValidations = new UserValidations();
+
+const upload = multer(uploadSettings);
 
 usersRouter.get('/', isAuthenticated, usersController.index);
 
 usersRouter.post('/', userValidations.create(), usersController.create);
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
 
 export default usersRouter;
