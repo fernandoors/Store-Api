6e370ec972a283dcac2391232e891a41ab250654
fernandoors <fernando.ors@outlook.com>
2022-01-03 17:09:43 -0300

    feat create user avatar controller

---
+++ src/modules/users/controllers/UserAvatarController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+export default class UserAvatarController {
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file?.filename as string,
+    });
+
+    return response.status(201).json(user);
+  }
+}
