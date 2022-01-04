1d141b17b17ffcba62023f1d1bc3e37b01e0ffe9
fernandoors <fernando.ors@outlook.com>
2022-01-04 17:15:58 -0300

    feat improve and create entities for order_product

---
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Customer from '@modules/customers/typeorm/entities/Customer';
+import OrdersProducts from './OrdersProducts';
+
+@Entity('orders')
+export default class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => Customer)
+  @JoinColumn({ name: 'customer_id' })
+  customer: Customer;
+
+  @OneToMany(() => OrdersProducts, ({ order }) => order, { cascade: true })
+  order_products: OrdersProducts[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
---
+++ src/modules/orders/typeorm/entities/OrdersProducts.ts
@@ -0,0 +1,37 @@
+import Product from '@modules/products/typeorm/entities/Product';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import Order from './Order';
+
+@Entity('orders_products')
+export default class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @ManyToOne(() => Order, order => order.order_products)
+  @JoinColumn({ name: 'order_id' })
+  order: Order;
+
+  @ManyToOne(() => Product, order => order.order_products)
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
