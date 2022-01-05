ea0830f298c6940aabde8588ee3c39646d94351f
fernandoors <fernando.ors@outlook.com>
2022-01-04 22:24:18 -0300

    feat update product repository to find multiple ID

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,10 +1,22 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, In, Repository } from 'typeorm';
 import Product from '../entities/Product';
 
+interface IProductsAll {
+  products: Product[];
+}
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
   public async findByName(name: string): Promise<Product | undefined> {
     const product = await this.findOne({ where: { name } });
     return product;
+  }
+  public async findAllByIds({ products }: IProductsAll): Promise<Product[]> {
+    const productsIds = products.map(product => product.id);
+    const existProducts = await this.find({
+      where: {
+        id: In(productsIds),
+      },
+    });
+    return existProducts;
   }
 }
