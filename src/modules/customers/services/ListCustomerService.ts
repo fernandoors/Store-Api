7bee116cdef9728617b6aa5a5c691aa4dce7c2d0
fernandoors <fernando.ors@outlook.com>
2022-01-05 21:28:22 -0300

    feat add pagination on list customer service

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,12 +1,16 @@
 import { getCustomRepository } from 'typeorm';
+import { PaginationAwareObject } from 'typeorm-pagination/dist/helpers/pagination';
 import Customer from '../typeorm/entities/Customer';
 import { CustomersRepository } from '../typeorm/repositores/CustomersRepository';
 
+interface ICustomerWithPaginate extends PaginationAwareObject {
+  data: Customer[];
+}
 export default class ListCustomerService {
-  public async execute(): Promise<Customer[]> {
+  public async execute(): Promise<ICustomerWithPaginate> {
     const customerRepository = getCustomRepository(CustomersRepository);
 
-    const customers = customerRepository.find();
+    const customers = await customerRepository.createQueryBuilder().paginate();
 
     return customers;
   }
