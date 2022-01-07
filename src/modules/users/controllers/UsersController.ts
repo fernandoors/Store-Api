9909fc5e789e100efd1bc62c768a2bf51316cbfa
fernandoors <fernando.ors@outlook.com>
2022-01-07 15:27:19 -0300

    fix applyed class transform in users list

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -8,7 +8,7 @@ export default class UserController {
 
     const users = await listUser.execute();
 
-    return response.json(users);
+    return response.json(classToClass(users));
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
