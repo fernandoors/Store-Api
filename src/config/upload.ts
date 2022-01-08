eaa226ee11a3c1febf9fb084b76db513dd7b3c38
fernandoors <fernando.ors@outlook.com>
2022-01-08 19:39:21 -0300

    fix set a temp folder for new files

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,11 +3,13 @@ import multer from 'multer';
 import path from 'path';
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 const uploadSettings = {
+  tmpFolder,
   directory: uploadFolder,
   storage: multer.diskStorage({
-    destination: uploadFolder,
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
