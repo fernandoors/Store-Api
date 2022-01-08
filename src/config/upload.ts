4e50005c48bb2ecadd582d7b179f4a94e22f67f3
fernandoors <fernando.ors@outlook.com>
2022-01-08 20:10:16 -0300

    fix set upload file to allow S3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,21 +1,37 @@
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
-
+interface IUploadSettings {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  directory: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    aws: { bucket: string };
+  };
+}
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
 const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 const uploadSettings = {
+  driver: process.env.STORAGE_DRIVER || 'disk',
   tmpFolder,
   directory: uploadFolder,
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
-      callback(null, fileName);
-    },
-  }),
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
+        callback(null, fileName);
+      },
+    }),
+  },
+  config: {
+    aws: { bucket: 'api-store-fdoors-lab' },
+  },
 };
 
-export default uploadSettings;
+export default uploadSettings as IUploadSettings;
